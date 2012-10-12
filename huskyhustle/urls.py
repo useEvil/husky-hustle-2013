790d58b4a704be9fc46fdf24460e24fe5a6d1b40
Thai Nguyen <useEvil@gmail.com>
2012-10-12 13:00:49 -0700

    - added debug condition for admin paths

--- huskyhustle/urls.py
+++ huskyhustle/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import patterns, include, url
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
+from django.conf import settings
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -35,9 +36,10 @@ urlpatterns = patterns('',
     url(r'^accounts/', include('registration.backends.default.urls')),
     url(r'^social/', include('socialregistration.urls', namespace = 'socialregistration')),
 
-    # Uncomment the admin/doc line below to enable admin documentation:
-    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    if settings.DEBUG:
+        # Uncomment the admin/doc line below to enable admin documentation:
+        url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
-    # Uncomment the next line to enable the admin:
-    url(r'^admin/', include(admin.site.urls)),
+        # Uncomment the next line to enable the admin:
+        url(r'^admin/', include(admin.site.urls)),
 )
