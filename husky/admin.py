c28b2ccbb4a5f5413e600db6bbb1b176089d1e7e
Thai Nguyen <useevil@gmail.com>
2012-10-16 16:13:07 -0700

    - changed Admin views  - allowed for inline editing

--- husky/admin.py
+++ husky/admin.py
@@ -7,8 +7,10 @@ from django.contrib.auth.models import User
 
 
 class ChildrenAdmin(admin.ModelAdmin):
-    fields = ['first_name', 'last_name', 'teacher', 'room_number', 'identifier', 'laps', 'date_added']
-    list_display = ['first_name', 'last_name', 'teacher', 'room_number', 'identifier', 'laps']
+    fields = ['parent', 'first_name', 'last_name', 'teacher', 'room_number', 'identifier', 'laps', 'date_added']
+    list_display = ['parent', 'first_name', 'last_name', 'teacher', 'room_number', 'identifier', 'laps']
+    search_fields = ['teacher', 'first_name', 'last_name', 'room_number', 'parent__first_name', 'parent__last_name']
+    list_editable = ['laps']
 
 class ChildrenInline(admin.StackedInline):
     model = Children
@@ -23,6 +25,7 @@ class ParentAdmin(admin.ModelAdmin):
     fields = ['first_name', 'last_name', 'email_address', 'phone_number', 'activation_key', 'date_added']
     list_display = ['id', 'first_name', 'last_name', 'email_address', 'phone_number', 'activation_key', 'user', 'facebook', 'twitter', 'num_chilren']
     inlines = [ChildrenInline]
+    search_fields = ['email_address', 'first_name', 'last_name']
 
 class BlogAdmin(admin.ModelAdmin):
     fields = ['title', 'content', 'author', 'date_added']
@@ -35,6 +38,8 @@ class MessageAdmin(admin.ModelAdmin):
 class DonationAdmin(admin.ModelAdmin):
     fields = ['child', 'first_name', 'last_name', 'email_address', 'phone_number', 'donation', 'per_lap', 'date_added', 'paid']
     list_display = ['child', 'first_name', 'last_name', 'email_address', 'phone_number', 'donation', 'laps', 'per_lap', 'total', 'date_added', 'paid']
+    search_fields = ['email_address', 'first_name', 'last_name', 'child__first_name', 'child__last_name']
+    list_editable = ['per_lap', 'donation', 'paid']
 
 class UserAdmin(UserAdmin):
     list_display = ['username', 'email', 'is_active', 'parent']
